99758f31c23e30807ddc86c542bb3acbefaf7e84
Niels Overwijn <niels.overwijn@gmail.com>
2018-08-18 15:21:09 +0200

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -1,41 +1,43 @@
-#include <RF24/RF24.h>
-#include <RF24Network/RF24Network.h>
+#include "RF24/RF24.h"
+#include "RF24Network/RF24Network.h"
 #include "spdlog/spdlog.h"
-#include "spdlog/sinks/daily_file_sink.h"
+#include "bcm2835.h"
 
-RF24 radio(RPI_V2_GPIO_P1_15, BCM2835_SPI_CS0, BCM2835_SPI_SPEED_8MHZ);
-RF24Network network(radio);
+int main () {
+  RF24 radio(RPI_V2_GPIO_P1_15, BCM2835_SPI_CS0, BCM2835_SPI_SPEED_8MHZ);
+  RF24Network network(radio);
 
-const uint16_t this_node = 00;
+  const uint16_t this_node = 00;
 
-struct payload_t {
-  uint16_t id;
-  uint16_t temperature;
-  uint16_t humidity;
-};
+  struct payload_t {
+    uint16_t id;
+    uint16_t temperature;
+    uint16_t humidity;
+  };
 
-radio.begin();
-delay(5);
-network.begin(90, this_node);
-radio.printDetails();
+  radio.begin();
+  delay(5);
+  network.begin(90, this_node);
+  daily_logger->info(radio.printDetails());
 
-auto daily_logger = spdlog::daily_logger_mt("daily_logger", "logs/daily.log", 0, 0);
-spdlog::set_pattern("[%H:%M:%S %z] [%n] %v");
+  auto daily_logger = spdlog::daily_logger_mt("daily_logger", "logs/daily.log", 0, 0);
+  spdlog::set_pattern("[%H:%M:%S %z] [%n] %v");
 
-daily_logger->info("Start logging");
+  daily_logger->info("APP STARTING");
 
-while(1) {
-  network.update();
-  while ( network.available() ) {
-    RF24NetworkHeader header;
+  while(1) {
+    network.update();
+    while ( network.available() ) {
+      RF24NetworkHeader header;
+      
+      payload_t payload;
+      network.read(header, &payload, sizeof(payload));
     
-    payload_t payload;
-    network.read(header, &payload, sizeof(payload));
-  
-    float f_temperature = (float)payload.temperature / 10;
-    float f_humidity = (float)payload.humidity / 10;
-    
-    daily_logger->info("id: {0:d}, temperature: {1:f}, humidity: {2:f}", payload.id, f_temperature, f_humidity);
+      float f_temperature = (float)payload.temperature / 10;
+      float f_humidity = (float)payload.humidity / 10;
+      
+      daily_logger->info("NODE:{0:d} TEMP:{1:f} HUM:{2:f}", payload.id, f_temperature, f_humidity);
+    }
+    delay(2000);
   }
-  delay(2000);
 }
