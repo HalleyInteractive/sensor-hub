ba51116c4b5b3e8042456fbaa519b84168d5a4f5
Niels Overwijn <niels.overwijn@gmail.com>
2018-09-01 09:42:20 +0200

    File cleanup

--- main.cpp
+++ main.cpp
@@ -22,7 +22,6 @@ struct payload_t {
 int main () {
 
   spdlog::set_pattern("[%H:%M:%S %z] [%n] %v");
-  // spdlog::set_async_mode(4096);
   spdlog::flush_on(spdlog::level::info);
 
   auto debug_logger = spdlog::create_async<spdlog::sinks::basic_file_sink_mt>("debug", "logs/debug.log");
@@ -30,41 +29,35 @@ int main () {
 
   debug_logger->info("Starting radio");
   try {
-    debug_logger->info("Starting begin");
+    debug_logger->info("Radio begin");
     radio.begin();
   } catch (int e) {
     debug_logger->warn("Could not start the radio, sudo permissions?");
-    std::cout << "Could not start the radio. " << e << std::endl;
   }
 
-  debug_logger->info("Radio started");
-
   std::this_thread::sleep_for(std::chrono::seconds(5));
-  debug_logger->info("Delayed 5 seconds");
+  debug_logger("Start network, 90");
   network.begin(90, this_node);
-  debug_logger->info("Network starting");
-  
-  // std::stringstream ss;
-  // auto old_buf = std::cout.rdbuf(ss.rdbuf()); 
   radio.printDetails();
-  // std::cout.rdbuf(old_buf);
-  // debug_logger->info(ss.str());
-  debug_logger->info("Radio details printed");
 
-  std::cout << "Network started, listening for data" << std::endl;
+  debug_logger->info("Network started, listening for data");
 
   while(1) {
     network.update();
     while ( network.available() ) {
-      debug_logger->info("Network available");
-      RF24NetworkHeader header;
+      try {
+        RF24NetworkHeader header;
 
-      payload_t payload;
-      network.read(header, &payload, sizeof(payload));
+        payload_t payload;
+        network.read(header, &payload, sizeof(payload));
 
-      float f_temperature = (float)payload.temperature / 10;
-      float f_humidity = (float)payload.humidity / 10;
-      // std::cout << "NODE:" << payload.id << " TEMP:" << f_temperature << " HUM:" << f_humidity;
+        float f_temperature = (float)payload.temperature / 10;
+        float f_humidity = (float)payload.humidity / 10;
+      } catch (...) {
+        debug_logger->error("Error reading network payload");
+      }
+      
+
       debug_logger->info("NODE:{0:d} TEMP:{1:.2f} HUM:{2:.2f}", payload.id, f_temperature, f_humidity);
       sensor_logger->info("NODE:{0:d} TEMP:{1:.2f} HUM:{2:.2f}", payload.id, f_temperature, f_humidity);
     }
