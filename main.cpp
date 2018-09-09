dfccc70bbc38e5a794f886ab778dc1e610a4ff46
Niels Overwijn <niels.overwijn@gmail.com>
2018-09-09 10:04:18 +0200

    Updates to main

--- main.cpp
+++ main.cpp
@@ -12,7 +12,9 @@
 
 RF24 radio(RPI_V2_GPIO_P1_15, BCM2835_SPI_CS0, BCM2835_SPI_SPEED_8MHZ);
 RF24Network network(radio);
-const uint16_t this_node = 00;
+const uint16_t NODE_ADDRESS = 00;
+const uint16_t CHANNEL = 90;
+const string BASE_DIR = "/home/pi/";
 struct payload_t {
   uint16_t id;
   uint16_t temperature;
@@ -24,8 +26,8 @@ int main () {
   spdlog::set_pattern("[%H:%M:%S %z] [%n] %v");
   spdlog::flush_on(spdlog::level::info);
 
-  auto debug_logger = spdlog::create_async<spdlog::sinks::basic_file_sink_mt>("debug", "logs/debug.log");
-  auto sensor_logger = spdlog::create_async<spdlog::sinks::basic_file_sink_mt>("sensor", "logs/sensor.log");
+  auto debug_logger = spdlog::create_async<spdlog::sinks::basic_file_sink_mt>("debug", BASE_DIR + "logs/debug.log");
+  auto sensor_logger = spdlog::create_async<spdlog::sinks::basic_file_sink_mt>("sensor", BASE_DIR + "logs/sensor.log");
 
   debug_logger->info("Starting radio");
   try {
@@ -36,8 +38,8 @@ int main () {
   }
 
   std::this_thread::sleep_for(std::chrono::seconds(5));
-  debug_logger("Start network, 90");
-  network.begin(90, this_node);
+  debug_logger("Start network on channel 90, address 00");
+  network.begin(CHANNEL, NODE_ADDRESS);
   radio.printDetails();
 
   debug_logger->info("Network started, listening for data");
@@ -56,9 +58,7 @@ int main () {
       } catch (...) {
         debug_logger->error("Error reading network payload");
       }
-      
 
-      debug_logger->info("NODE:{0:d} TEMP:{1:.2f} HUM:{2:.2f}", payload.id, f_temperature, f_humidity);
       sensor_logger->info("NODE:{0:d} TEMP:{1:.2f} HUM:{2:.2f}", payload.id, f_temperature, f_humidity);
     }
     std::this_thread::sleep_for(std::chrono::seconds(2));
